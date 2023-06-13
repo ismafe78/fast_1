22a6d41f35f535b9f205a6106d47cc1e2044f706
ismafe78 <47546093+ismafe78@users.noreply.github.com>
2023-06-13 12:44:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from typing import Optional
 
 from fastapi import FastAPI
@@ -12,3 +13,13 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/get_max_duration/{anio}/{plataforma}/{dtype}')
+def get_max_duration(anio: int, plataforma: str, dtype: str):
+    
+    df_m_d=pd.read_csv("duracion_max_pelicula.cvs")
+    duracion_maxima = df_m_d[(df_m_d["release_year"]==anio) 
+       & (df_m_d["plataforma"]==plataforma)
+        & (df_m_d["duration_type"]==dtype)]["title"].max()
+    
+    return {'pelicula': duracion_maxima}

a87b0c95a96bd0b7500e1072d67d993b3e31f506
ismafe78 <47546093+ismafe78@users.noreply.github.com>
2023-10-28 01:06:41 -0600

    main.py

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+    
+@app.get('/developer/{desarrollador}')
+def developer(desarollador):
+  df=pd.read_csv("apiFreeDeveloper.csv")
+  total_datos = df[(df['developer']=='CAPCOM Co., Ltd.')].groupby(["year"]).count()["developer"]
+  datos_free = df[(df['developer']=='CAPCOM Co., Ltd.')&(df['price']=="Free")].groupby(["year"]).count()["developer"]
+  df_1=pd.DataFrame(total_datos)
+  df_2=pd.DataFrame(datos_free)
+  
+  
+  df_2.reset_index(inplace=True)
+  df_1.reset_index(inplace=True)
+
+  df_t=df_1.merge(df_2, left_on='year', right_on='year')
+  mul=df_t["developer_y"]*100
+  df_t['Contenido Free']=mul.div(df_t["developer_x"])
+
+  return df_t
 
 @app.get('/get_max_duration/{anio}/{plataforma}/{dtype}')
 def get_max_duration(anio: int, plataforma: str, dtype: str):
